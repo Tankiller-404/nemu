4a072bc558aa0b7bccd9ccee1017f51203b29b17
Tankiller-404 <2531054860@qq.com>
2026-09-16 20:51:47 +0800

    PA2 必做任务二回归修正：补全int 0x80及test立即数映射

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -14,6 +14,26 @@ make_helper(int3) {
 	return 1;
 }
 
+make_helper(int_i_b) {
+	uint8_t vector = instr_fetch(eip + 1, 1);
+	uint32_t syscall = cpu.eax;
+
+	Assert(vector == 0x80, "unsupported interrupt vector 0x%02x", vector);
+	if (syscall == 4 && (cpu.ebx == 1 || cpu.ebx == 2)) {
+		uint32_t i;
+		for (i = 0; i < cpu.edx; i ++) {
+			putchar(swaddr_read(cpu.ecx + i, 1));
+		}
+		fflush(stdout);
+		cpu.eax = cpu.edx;
+	} else {
+		cpu.eax = (uint32_t)-1;
+	}
+
+	print_asm("int $0x%02x", vector);
+	return 2;
+}
+
 make_helper(lea) {
 	ModR_M m;
 	m.val = instr_fetch(eip + 1, 1);
